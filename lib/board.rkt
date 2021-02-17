eb80682b0e68901ee7042745be4ab0ca2f5bfe01
HaydenTheBaron <hlebaron98@gmail.com>
2021-02-16 20:04:01 -0700

    Implement board query functions in board.rkt

--- lib/board.rkt
+++ lib/board.rkt
@@ -4,9 +4,60 @@ of a Santorini game in a single snapshot. A board knows about:
 - the position of the players' tokens
 - which player's turn it is
 - the position and level of each tower.
-- the total number of elapsed turns since the start of the game. |#
+- the total number of elapsed turns since the start of the game.
+
+This file also contains functions for querying the board. Such
+functions are named boardq-*. |#
 
 #lang racket
 
-;; TODO: define board model type
 
+
+;; * A board is a dictionary with three keys:
+;;   * "turn" mapped to a nonnegative integer, where 0 corresponds to the first turn of the game
+;;   * "spaces" mapped to an array of 5 rows in order
+;;   * "players" mapped to an array of 2 players, where the first player in the array takes the next turn
+;; * A row is an array of 5 levels, one for each column in order
+;; * A level is 0, 1, 2, 3, or 4
+;; * A player is an array of 2 spaces, where the order of the spaces in the array does not matter
+;; * A space is an array of 2 dimensions, row then column
+;; * A dimension is 1, 2, 3, 4, or 5
+
+
+(define (boardq-turn board)
+  (hash-ref board 'turn))
+
+(define (boardq-spaces board)
+  (hash-ref board 'spaces))
+
+(define (boardq-players board)
+  (hash-ref board 'players))
+
+(define (boardq-my-tokens board)
+  (list-ref (hash-ref board 'players) 0))
+
+(define (boardq-enemy-tokens board)
+  (list-ref (hash-ref board 'players) 1))
+
+;; space ordered pair is 1-idxed
+(define (boardq-levelof-space board row col)
+  (list-ref (list-ref (boardq-spaces board) row) col))
+
+(define (boardq-space-capped? board row col)
+  (= (boardq-levelof-space board row col) 4))
+
+
+;; space ordered pair is 1-idxed
+(define (boardq-adjacent-spaces board row col)
+  (filter
+   (lambda (l) (not (or (= (list-ref l 0) 0)  (= (list-ref l 0) 6)
+                        (= (list-ref l 1) 0)  (= (list-ref l 1) 6))))
+   (list
+    `(,(+ row 1) ,col)
+    `(,(+ row 1) ,(+ col 1))
+    `(,row ,(+ col 1))
+    `(,(- row 1) ,(+ col 1))
+    `(,(- row 1) ,col)
+    `(,(- row 1) ,(- col 1))
+    `(,row ,(- col 1))
+    `(,(+ row 1) ,(- col 1)))))
