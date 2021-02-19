918c8435636d8d32874d5f4cee37564ebc021b78
HaydenTheBaron <hlebaron98@gmail.com>
2021-02-19 10:21:13 -0700

    Write tests for another board ..movestep

--- test/test-cspsolve-movestep.rkt
+++ test/test-cspsolve-movestep.rkt
@@ -7,7 +7,16 @@
 
 
 (define board1 (string->jsexpr
-                "{\"players\":[[[2, 3], [4, 4]], [[5, 2], [5, 3]]],
+                "{\"players\":[[[2, 3], [4, 4]], [[2, 5], [3, 5]]],
+                  \"spaces\":[[0, 0, 0, 0, 2],
+                              [1, 1, 2, 0, 0],
+                              [1, 0, 0, 3, 0],
+                              [0, 0, 3, 0, 0],
+                              [0, 0, 0, 1, 4]],
+                  \"turn\": 18}"))
+
+(define board2 (string->jsexpr
+                "{\"players\":[[[2, 5], [3, 5]], [[3, 3], [4, 4]]],
                   \"spaces\":[[0, 0, 0, 0, 2],
                               [1, 1, 2, 0, 0],
                               [1, 0, 0, 3, 0],
@@ -16,11 +25,7 @@
                   \"turn\": 18}"))
 
 
-(define board2 (string->jsexpr
-                "{\"players\":[],
-                  \"spaces\":[],
-                  \"turn\": 18}"
-                ))
+
 
 (define board3 (string->jsexpr
                 "{\"players\":[],
@@ -58,15 +63,51 @@
             board1))
               (set '(1 2) '(1 3) '(1 4) '(2 2) '(2 4) '(3 2) '(3 3) '(3 4)))
 
+
+
+
 (check-equal? (list->set (get-spaces-tok-can-move-to
                           (cadr (boardq-my-tokens board1)) ;(4 4)
                           (car (boardq-my-tokens board1))     ;; '(3 2)
                           (car (boardq-enemy-tokens board1))
                           (cadr (boardq-enemy-tokens board1))
                           board1))
-              (set '(3 3) '(3 5) '(4 5) '(5 4)))
+              (set '(3 3) '(4 5) '(5 4) '(5 3)))
 
-(check-eq? (length (cspsolve-movestep board1)) 12)
+(check-equal? (length (cspsolve-movestep board1)) 12)
 
 
 ;(cspsolve-movestep board1)
+
+
+
+
+(check-equal? (list->set (get-spaces-tok-can-move-to
+                          (car (boardq-my-tokens board2))
+                          (cadr (boardq-my-tokens board2))
+                          (car (boardq-enemy-tokens board2))
+                          (cadr (boardq-enemy-tokens board2))
+                          board2))
+              (set '(1 4) '(2 4)))
+
+
+
+
+(check-equal? (list->set (get-spaces-tok-can-move-to
+                          (cadr (boardq-my-tokens board2))
+                          (car (boardq-my-tokens board2))
+                          (car (boardq-enemy-tokens board2))
+                          (cadr (boardq-enemy-tokens board2))
+                          board2))
+              (set '(2 4) '(4 5)))
+
+(check-equal? (length (cspsolve-movestep board1)) 12)
+(check-equal? (length (cspsolve-movestep board2)) 4)
+
+;; (check-equal? (andmap
+;;                (λ (brd)
+;;                  (= 19 (boardq-turn brd)))
+;;                (cspsolve-movestep board1))
+;;                #t)
+
+;(cspsolve-movestep board2)
